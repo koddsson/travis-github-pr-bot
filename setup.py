30c41ea6a8aee0a5a004a4d1a59148e09e7c16b9
Kristján Oddsson <koddsson@gmail.com>
2014-11-25 15:41:29 -0800

    MOAR FIXES :boat:

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-    name='travis-bot',
-    packages=['travis-bot'],  # this must be the same as the name above
+    name='travis_bot',
+    packages=['travis_bot'],  # this must be the same as the name above
     version='0.1',
     description='A simple API call to comment message on PR from Travis',
     author='koddsson',
@@ -10,4 +10,5 @@ setup(
     download_url='https://github.com/koddsson/travis-github-pr-bot/tarball/0.1',
     keywords=['travis', 'bot', 'github'],
     classifiers=[],
+    scripts=['travis_bot/travis_bot.py'],
 )
