1d877906c36ea752745a82a62bb99b0998ad8932
Kristján Oddsson <koddsson@gmail.com>
2014-11-25 15:09:49 -0800

    Continues deployment with pypi and travis!

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='travis-bot',
+    packages=['travis-bot'],  # this must be the same as the name above
+    version='0.1',
+    description='A simple API call to comment message on PR from Travis',
+    author='koddsson',
+    author_email='koddsson@gmail.com',
+    url='https://github.com/koddsson/travis-github-pr-bot',
+    download_url='https://github.com/koddsson/travis-github-pr-bot/tarball/0.1',
+    keywords=['travis', 'bot', 'github'],
+    classifiers=[],
+)
