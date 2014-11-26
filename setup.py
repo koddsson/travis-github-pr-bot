b3d9d9645c3681046f98cc8951f4d3214726f81d
Kristján Oddsson <koddsson@gmail.com>
2014-11-25 16:27:25 -0800

    bump

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='travis_bot',
     packages=['travis_bot'],  # this must be the same as the name above
-    version='0.1',
+    version='0.1.1',
     description='A simple API call to comment message on PR from Travis',
     author='koddsson',
     author_email='koddsson@gmail.com',
     url='https://github.com/koddsson/travis-github-pr-bot',
-    download_url='https://github.com/koddsson/travis-github-pr-bot/tarball/0.1',
+    download_url='https://github.com/koddsson/travis-github-pr-bot/tarball/0.1.1',
     keywords=['travis', 'bot', 'github'],
     classifiers=[],
     scripts=['travis_bot/travis_bot.py'],
